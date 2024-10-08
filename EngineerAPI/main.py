cfda7fcee834226c7eceaffb2ee24a94e9e6cf6e
Dylan Nicolay <108036935+Nicolax0@users.noreply.github.com>
2024-10-08 16:54:47 -0400

    Merged Changes

--- EngineerAPI/main.py
+++ EngineerAPI/main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from verification import *
 import os
 
 TOKEN = os.getenv("DISCORD_KEY")
@@ -9,7 +10,6 @@ CHANNEL = int(os.getenv("CHANNEL_ID"))
 # Define intents
 intents = discord.Intents.default()
 intents.message_content = True  # Necessary for reading message content
-intents.members = True
 
 # Create an instance of Bot with a command prefix
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -22,7 +22,17 @@ async def on_ready():
 async def ping(ctx):
     if ctx.channel.id == CHANNEL:
         await ctx.send('pong!')
-        
+
+@bot.command(name='verify')
+async def verify(ctx, RCSID: str = None):
+    if not RCSID:
+        await ctx.send("Please execute the command in the format !verify <RCSID>")
+        return
+    result = send_verification_code(RCSID, ctx.author.id)
+    await ctx.send(f"Email sent to {RCSID + "@rpi.edu"}!\nStatus Code: {str(result)}")
+
+bot.run(TOKEN)
+
 @bot.command(name='graduate')
 async def ping(ctx, member: discord.Member = None):
     studentRole = discord.utils.get(ctx.guild.roles, name='Student')
@@ -52,7 +62,7 @@ async def ping(ctx, member: discord.Member = None):
         membersProcessed = 0
         async with ctx.typing():
             for member in ctx.guild_members:
-                if student_role in member_roles:
+                if studentRole in member.roles:
                     try:
                         await member.remove_roles(studentRole, reason="Graduated")
                         await member.add_roles(alumniRole, reason='Graduated')
@@ -62,5 +72,24 @@ async def ping(ctx, member: discord.Member = None):
                     except Exception as e:
                         await ctx.send(f"Failed to update roles for {member.display_name}: {e}")
         await ctx.send(f"Processed {membersProcessed} members.")
-                        
+
+@bot.command(name='createrole')
+async def ping(ctx, roleName: str):
+    checkRole = discord.utils.get(ctx.guild.roles, name=roleName)
+    if checkRole:
+        await ctx.send(f"This role already exists.")
+    else:
+        newRole = await ctx.guild.create_role(name=roleName, reason=f'Role created by {ctx.author.name}')
+        await ctx.send(f"Role {newRole.name} has been created.")
+            
+@bot.command(name='deleterole')
+async def ping(ctx, roleName: str):
+    checkRole = discord.utils.get(ctx.guild.roles, name=roleName)
+    if not checkRole:
+        await ctx.send(f"This role doesn't exist.")
+    else:
+        await checkRole.delete(reason=f'Role deleted by {ctx.author.name}')
+        await ctx.send(f"Role {roleName} has been deleted.")
+
+        
 bot.run(TOKEN)
